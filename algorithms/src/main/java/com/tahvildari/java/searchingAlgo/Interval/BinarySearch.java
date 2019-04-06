6dce7872a8fcce0ad1ff907face9f5ffe803177b
baktash tahvildari <develop.tahvildari@gmail.com>
2019-04-06 02:20:25 -0700

    Linear Search and Binary Search Algorithms is added

---
+++ algorithms/src/main/java/com/tahvildari/java/searchingAlgo/Interval/BinarySearch.java
@@ -0,0 +1,49 @@
+package com.tahvildari.java.searchingAlgo.Interval;
+
+/**
+ * The auxiliary space of this algorithm  in recursive implementation is log(n)
+ *
+ */
+public class BinarySearch {
+
+    // This method search value in given sorted array , if value if equal to middle element
+    // return index; otherwise if value if greater of middle element it search for element in right
+    // half of array , otherwise search for value in the left hand of array
+    //paradigm:  Decrease and Conquer.
+    static int binarySearch(int[] array, int leftIndex, int rightIndex, int value) {
+        int middle = 0;
+
+        if (rightIndex > 1) {
+            middle = leftIndex + (rightIndex - 1) / 2;
+
+            if (array[middle] == value)
+                return middle;
+
+            if (array[middle] < value) {
+
+                return binarySearch(array, middle + 1, rightIndex, value);
+            }
+
+            return binarySearch(array, leftIndex, middle - 1, value);
+
+
+        }
+
+        return -1;
+
+    }
+
+    //Driver method to test
+    public static void main(String args[]) {
+        int[] array = {1, 5, 7, 12, 13, 25, 27};
+        int value = 7;
+        int index = binarySearch(array, 0, array.length, value);
+
+        if (index == -1)
+            System.out.println("The value is not existed in the given array");
+        else
+            System.out.println("the value is presented at the index =  " + index);
+
+
+    }
+}
---
+++ algorithms/src/main/java/com/tahvildari/java/searchingAlgo/sequential/LinearSearch.java
@@ -0,0 +1,32 @@
+package com.tahvildari.java.searchingAlgo.sequential;
+
+/**
+ * The complexity of this algorithm is O(n) and it has less usage
+ */
+public class LinearSearch {
+    public static int search(int[] array , int x)
+    {
+        int size = array.length;
+        for(int i =0; i < size ; i++)
+        {
+            if(array[i] == x)
+                return x;
+        }
+        return -1;
+
+    }
+
+
+    public static void main(String args[])
+    {
+        int[] array = {1,7,9,12,3,12};
+        int value = 3;
+        int index = search(array, value);
+        if(index == -1 )
+            System.out.println("The value is not existed in the given array");
+        else
+            System.out.println("the value is presented at the index =  " + index);
+
+
+    }
+}
